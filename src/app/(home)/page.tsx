92f4bfa59ee752bcdaeb278a0409cfeab2315c52
Lucas Campos <lucasoliveiracampos81@gmai.com>
2023-10-18 14:50:14 -0300

    feat: add third banner in home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,3 @@
-
 import Image from "next/image";
 import Categories from "./components/categories";
 import { prismaClient } from "@/lib/prisma";
@@ -7,33 +6,33 @@ import SectionTitle from "./components/section-title";
 import PromoBanner from "./components/promo-banner";
 
 export default async function Home() {
-const deals = await prismaClient.product.findMany({
-  where: {
-    discountPercentage: {
-      gt: 0,
+  const deals = await prismaClient.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
     },
-  },
-})
+  });
 
   const keyboards = await prismaClient.product.findMany({
     where: {
       category: {
-        slug: "keyboards"
-      }
-    }
-  })
+        slug: "keyboards",
+      },
+    },
+  });
 
   return (
-    <div className="">
+    <div className="flex flex-col gap-8">
       <PromoBanner
         src="/banner-inicial-home-01.png"
         alt="Até 55% de desconto esse mês!"
       />
-      <div className="mt-8 px-5">
+      <div className="px-5">
         <Categories />
       </div>
-      <div className="mt-8">
-      <SectionTitle>Ofertas</SectionTitle>
+      <div>
+        <SectionTitle>Ofertas</SectionTitle>
         <ProductList products={deals} />
       </div>
       <PromoBanner
@@ -44,6 +43,12 @@ const deals = await prismaClient.product.findMany({
       <div className="mt-3">
         <SectionTitle>Teclados</SectionTitle>
         <ProductList products={keyboards} />
+      </div>
+      <div>
+        <PromoBanner
+          src="/banner-home-03.png"
+          alt="Até 55% de desconto em mouses!"
+        />
       </div>
     </div>
   );
