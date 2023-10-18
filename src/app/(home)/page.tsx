1905d995f4ee07760121a85f94963fe5d0302124
Lucas Campos <lucasoliveiracampos81@gmai.com>
2023-10-18 12:13:50 -0300

    feat: add Categories and CategoriesItem component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,15 +1,22 @@
 "use client";
 
 import Image from "next/image";
+import Categories from "./components/categories";
 
 export default function Home() {
-  return <div className="p-5">
-    <Image src="/banner-inicial-home-01.png"
-    height={0}
-    width={0}
-    alt="Até 55% de desconto esse mês!"
-    className="h-auto w-full"
-    sizes="100vw"
-     />
-  </div>;
+  return (
+    <div className="p-5">
+      <Image
+        src="/banner-inicial-home-01.png"
+        height={0}
+        width={0}
+        alt="Até 55% de desconto esse mês!"
+        className="h-auto w-full"
+        sizes="100vw"
+      />
+      <div className="mt-8">
+        <Categories />
+      </div>
+    </div>
+  );
 }
