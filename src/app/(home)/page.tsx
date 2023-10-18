c9113558b11dec5b2bde3e7befd75231ece82c25
Lucas Campos <lucasoliveiracampos81@gmai.com>
2023-10-18 14:33:41 -0300

    feat: add second banner in home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -27,8 +27,17 @@ const deals = await prismaClient.product.findMany({
         <Categories />
       </div>
       <div className="mt-8">
+        <p className="font-bold uppercase pl-5 mb-3 ">Ofertas</p>
         <ProductList products={deals} />
       </div>
+      <Image
+        src="/banner-home-02.png"
+        height={0}
+        width={0}
+        alt="Até 55% de desconto em mouses!"
+        className="h-auto w-full px-5 py-4"
+        sizes="100vw"
+      />
     </div>
   );
 }
