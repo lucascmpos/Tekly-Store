81d4ab2f894912ce452a38fb036bc284d0a57252
Lucas Campos <lucasoliveiracampos81@gmail.com>
2023-10-26 11:37:50 -0300

    fix: fixed success and cancel order url

--- src/actions/checkout.ts
+++ src/actions/checkout.ts
@@ -14,8 +14,8 @@ export const createCheckout = async (
   const checkout = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     mode: "payment",
-    success_url: 'http://localhost:3000',
-    cancel_url: 'http://localhost:3000',
+    success_url: "http://lctechstore.netlify.app",
+    cancel_url: "http://lctechstore.netlify.app",
     metadata: {
       orderId,
     },
