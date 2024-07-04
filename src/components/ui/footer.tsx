5a137e3dead894dc0ed8309dd3d7866cffb8aeee
lucascmpos <lucasoliveiracampos81@gmail.com>
2024-07-04 11:15:22 -0300

    style: footer is responsive on desktop

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -3,19 +3,21 @@ import { IoLogoGithub } from "react-icons/io";
 
 const Footer = () => {
   return (
-    <div className="flex items-center justify-between bg-accent px-8 py-4 text-[0.625rem] opacity-75">
-      <p>
-        © 2023 Copyright <span className="font-semibold">PeriWare Store</span>{" "}
-      </p>
+    <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75  lg:text-sm">
+      <div className="container mx-auto flex w-full items-center justify-between">
+        <p>
+          © 2023 Copyright <span className="font-semibold">Tekly Store</span>{" "}
+        </p>
 
-      <a
-        className="flex flex-row items-center justify-center gap-2 hover:text-purple-700"
-        href="https://github.com/lucascmpos/PeriWare-Store"
-        target="_blank"
-      >
-        <p>Feito por Lucas Campos</p>
-        <IoLogoGithub size={18} />
-      </a>
+        <a
+          className="flex flex-row items-center justify-center gap-2 hover:underline"
+          href="https://github.com/lucascmpos/PeriWare-Store"
+          target="_blank"
+        >
+          <p>Feito por Lucas Campos</p>
+          <IoLogoGithub size={18} />
+        </a>
+      </div>
     </div>
   );
 };
